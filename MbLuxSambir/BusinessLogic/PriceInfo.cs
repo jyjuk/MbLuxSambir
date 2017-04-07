84ca3d65385f9b795f73123e18f0a7419138b343
jyjuk <jyjuk@DESKTOP-ET7O13S>
2017-04-07 21:40:08 +0300

    Fill PriceInfo.cs

--- MbLuxSambir/BusinessLogic/PriceInfo.cs
+++ MbLuxSambir/BusinessLogic/PriceInfo.cs
@@ -6,13 +6,37 @@ namespace MbLuxSambir.BusinessLogic
     {
         public decimal GetPrice(string componentName)
         {
-            return 6;
+            return 9;
         }
 
         public Dictionary<string, decimal> GetAll()
         {
-            var dictionary = new Dictionary<string, decimal>() { { "name", 90 } };
+            var dictionary = new Dictionary<string, decimal>()
+            {
+                {"Плита", 150 },
+                {"DVP ",200 },
+                {"Нога кухонна",5 },
+                {"Шурупи",0.1m },
+                {"Конфірмат",2 },
+                {"Полко тримач",0.5m },
+                {"Стяжка міжсекційна",0.5m },
+                {"Фасад",540 },
+                {"Ручка",15 },
+                {"Ручка2",21 },
+                {"Завіс",3 },
+                {"Завіс дотяг",25 },
+                {"Направляюча",37 },
+                {"Стільниця",120 },
+                {"PCV",7 },
+                {"Клей PCV",30 },
+                {"Cargo",140 },
+                {"Карніз",27 },
+                {"Ліфт",10 },
+                {"Сушка",70 },
+                {"Петлі монтажні",0.5m }
+            };
             return dictionary;
         }
     }
 }
+
